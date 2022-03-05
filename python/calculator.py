af9babeee7bf92bda2c6992eed5fbaf3a08c4e59
dimker <energetic.ratomka@gmail.com>
2022-03-05 23:42:30 +0300

    main

---
+++ python/calculator.py
@@ -0,0 +1,21 @@
+#Калькулятор
+#color text
+from colorama import init
+from colorama import Fore, Back, Style
+init()
+print( Fore.BLACK )
+print( Back.GREEN )
+what = input( "operation (+,-): " )
+a = float( input( "first" ))
+b = float( input( "second" ))
+if what == "+":
+    c = a + b
+    print("Result" + str(c))
+elif what == "-":
+    c = a - b  
+    print("Result" + str(c))
+else:
+    print("Wrong operation")  
+
+    #pip install pyinstaller
+    
---
+++ python/test.py
@@ -0,0 +1,17 @@
+#int целые
+#float дробные
+#str строка
+name ="Dima"
+#bool True False
+#print \n перевод строки
+print ("Hello "+ name)
+name = input("input name")
+a = 5
+b = 20.5
+c = a+b
+print(c)
+print(round(b))
+import math
+print (math.floor(b))
+print (math.ceil(b))
+print(math.pi)
